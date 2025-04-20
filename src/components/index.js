fb73c144acf31ce361038d5e60e6e7ff5a17bd28
DEVRAJ2906 <devrajtadvi2906@gmail.com>
2025-04-20 21:43:25 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,34 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./Container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import Input from "./Input";
+import Select from './Select';
+import PostCard from "./PostCard";
+import Login from "./Login";
+import Signup from "./Signup";
+import Protected from "./AuthLayout";
+import RTE from "./RTE";
+import PostForm from "./post-form/PostForm";
+import AuthLayout from "./AuthLayout";
+
+
+export {
+    Header,
+    Footer,
+    Logo,
+    Container,
+    LogoutBtn,
+    Input,
+    Button,
+    Select,
+    PostCard,
+    Login,
+    Signup,
+    Protected,
+    AuthLayout,
+    RTE,
+    PostForm,
+}
